71ce8a24e8f3a7a3a957a16bcae6ff831d6c84fd
wenqi0425 <xiaoc288@gmail.com>
2022-12-17 12:16:50 +0100

    EFRecipeService

---
+++ Services/EFServices/EFRecipeService.cs
@@ -0,0 +1,56 @@
+﻿using Foodie.Models;
+using Foodie.Services.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Foodie.Services.EFServices
+{
+    public class EFRecipeService : IRecipeService
+    {
+        AppDbContext _context;
+
+        public EFRecipeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            _context.SaveChanges();
+        }
+
+        public void DeleteRecipe(Recipe recipe)
+        {
+            _context.Recipes.Remove(recipe);
+            _context.SaveChanges();
+        }
+
+        public void EditRecipe(Recipe recipe)
+        {
+            _context.Recipes.Update(recipe);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Recipe> GetAllRecipes()
+        {
+            return _context.Recipes;
+        }
+
+        public Recipe GetRecipeById(int recipeId)
+        {
+            return _context.Recipes.FirstOrDefault(r => r.Id == recipeId);
+        }
+
+        public IEnumerable<Recipe> GetRecipesByName(string recipeName)
+        {
+            IEnumerable<Recipe> recipes = _context.Recipes
+                .Where(r => r.Name == recipeName).ToList();
+
+            return recipes;
+        }
+    }
+}
---
+++ Services/Interfaces/IRecipeService.cs
@@ -0,0 +1,15 @@
+﻿using Foodie.Models;
+using System.Collections.Generic;
+
+namespace Foodie.Services.Interfaces
+{
+    public interface IRecipeService
+    {
+        void AddRecipe(Recipe recipe);
+        void DeleteRecipe(Recipe recipe);
+        void EditRecipe(Recipe recipe);
+        Recipe GetRecipeById(int recipeId);
+        IEnumerable<Recipe> GetAllRecipes();
+        IEnumerable<Recipe> GetRecipesByName(string recipeName);        
+    }
+}
