d5ecbeabcb4731ee94f52c5c5e2e440f5d1b7433
wenqi0425 <xiaoc288@gmail.com>
2022-12-27 22:27:29 +0100

    page modify

--- Areas/Identity/Pages/Account/Manage/DeleteMyRecipe.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/DeleteMyRecipe.cshtml.cs
@@ -1,4 +1,5 @@
 using Foodie.Models;
+using Foodie.Services.EFServices;
 using Foodie.Services.Interfaces;
 
 using Microsoft.AspNetCore.Authorization;
@@ -6,26 +7,45 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Foodie.Areas.Identity.Pages.Account.Manage
 {
     [Authorize]
+    [BindProperties]
     public class DeleteMyRecipeModel : PageModel
     {
-        IRecipeService _recipeService;
+        private IRecipeService _recipeService;
+        private IRecipeItemService _recipeItemService;
 
-        public DeleteMyRecipeModel(IRecipeService service)
+        public DeleteMyRecipeModel(IRecipeService recipeService, IRecipeItemService recipeItemService)
         {
-            _recipeService = service;
+            _recipeService = recipeService;
+            _recipeItemService = recipeItemService;
         }
 
-        [BindProperty] public Recipe Recipe { get; set; }
-        [BindProperty] public IList<RecipeItem> RecipeItems { get; set; }
+        public RecipeItem RecipeItem { get; set; }
+        public Recipe Recipe { get; set; }
+        public IList<RecipeItem> RecipeItems { get; set; }
 
-        public IActionResult OnGetAsync(int recipeId)
+        public RecipeItem RecipeItem1 { get; set; }
+        public RecipeItem RecipeItem2 { get; set; }
+        public RecipeItem RecipeItem3 { get; set; }
+        public RecipeItem RecipeItem4 { get; set; }
+        public RecipeItem RecipeItem5 { get; set; }
+
+        //public IActionResult OnGetAsync(int recipeId)
+        //{
+        //    Recipe = _recipeService.GetRecipeById(recipeId);
+        //    return Page();
+        //}
+
+        public async Task OnGetAsync(int recipeId) // must be the same as the asp-route-recipeId
         {
             Recipe = _recipeService.GetRecipeById(recipeId);
-            return Page();
+            RecipeItems = _recipeItemService.GetRecipeItemsByRecipeId(recipeId).ToList();
+            //return Page();
         }
 
         public IActionResult OnPost(Recipe recipe)
