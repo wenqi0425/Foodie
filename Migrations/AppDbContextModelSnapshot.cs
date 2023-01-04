41f0b8c9b466700f7066ab57e32e8849da135cb9
wenqi0425 <xiaoc288@gmail.com>
2023-01-04 22:15:32 +0100

    commit from another computer

--- Migrations/AppDbContextModelSnapshot.cs
+++ Migrations/AppDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Foodie.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.32")
+                .HasAnnotation("ProductVersion", "3.1.13")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +13,7 @@ namespace Foodie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); // web application
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
